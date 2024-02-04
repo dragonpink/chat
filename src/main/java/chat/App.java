9be81a9c378aa882f7732a1128cba986348ab9ee
sonja <sonja@Schneggchen.fritz.box>
2024-02-04 17:59:43 +0100

    2nd try

--- src/main/java/chat/App.java
+++ src/main/java/chat/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Ja ich suche den fehler");
+        System.out.println(" Ja ich suche den fehler ");
     }
 }
